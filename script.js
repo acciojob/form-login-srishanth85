b752b02bafc4191f945618f59fb01b3e143e4c2a
srishanth85 <srishanth8085@gmail.com>
2025-12-25 14:21:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
+	const fname = document.getElementsByName("fname")[0].value;
+		const lname = document.getElementsByName("lname")[0].value;
+	alert(fname+" "+lname);
     //Write your code here
-
 }
+// function getFormvalue() {
+//   const fname = document.getElementsByName("fname")[0].value;
+//   const lname = document.getElementsByName("lname")[0].value;
+
+//   alert(fname + " " + lname);
+// }
